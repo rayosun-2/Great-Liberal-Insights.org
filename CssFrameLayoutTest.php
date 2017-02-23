fcae705cd09ef7c6a35ff22dce6c36b55043b844
rayosun-2 <rayosun@gmail.com>
2017-02-23 15:50:43 -0500

    Add files via upload

---
+++ CssFrameLayoutTest.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<! -
+when I use an preliminary intro, this is http://LiberalsLikeChrist.OrgPage-Launchers/CssFontSizingTest.php   
+                      		using StyleSheet ="MyPhoneFrames-TESTER.css
+->
+<html>
+<head>
+<title>CssFontSizingTest.php</title>
+<meta charset="ISO-8859-1">
+
+<meta name="Author" content="Ray Dubuque">
+<LINK REL=StyleSheet HREF="MyPhoneFrames-TESTER.css" TYPE="text/css" MEDIA=all>
+
+</head>
+
+
+    <iframe id="gray-1" name="gray-1"  src="MyPhoneFrames-TESTER.css" >
+    <p>Your browser does not support iframes.</p>
+    </iframe>
+    
+    
+        <iframe id="red-2" name="red-2"  src="MyPhoneFrames-TESTER.css" >
+        <p>Your browser does not support iframes.</p>
+        </iframe>
+        
+            
+            <iframe id="blue-3" name="blue-3"  src="MyPhoneFrames-TESTER.css" >
+            <p>Your browser does not support iframes.</p>
+            </iframe>
+            
+            
+                <iframe id="purple-4" name="purple-4"  src="MyPhoneFrames-TESTER.css" >
+                <p>Your browser does not support iframes.</p>
+                </iframe>
+                
+                </body>
+                </html>
+                </html>
+            	
